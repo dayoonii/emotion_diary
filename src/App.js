7fa292009cdbffe300d89ee074b42734050b77c3
dayoonii <yoonee9829@naver.com>
2023-03-03 23:40:11 +0900

    프로젝트 기초공사1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import RouteTest from "./components/RouteTest";
-
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+//COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
+  // process.env가 작동안할 경우 사용되는 코드
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headeText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
         <h2>App.js</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
+        <MyButton
+          type={"skdfsdfsdf"}
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+        />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
-          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,50 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "dayoon" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 };
 
 export default Edit;
+
+// react Router v6
+// react에서 csr기반의 페이지 라우팅을 할 수 있게 해주는 라이브러리
+
+// 1. path Variable
+//  - 경로의 변수 사용
+// 2. Querry String
+// - 1번과 비슷하지만 url과 함께 변수를 전달할 수 있는 가장 쉽고 간단한 방법
+// - 웹페이지에 데이터를 전달하는 가장 간단한 방법
+// 3.Page Moving
+// - 페이지 이동
